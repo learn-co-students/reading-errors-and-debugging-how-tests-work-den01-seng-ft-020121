627d9984892d55190deec7315cc53510e186d80c
Marnie Robinson <marnielane@gmail.com>
2021-01-15 00:03:05 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
-# Add your variables here
+first_number = (1)
+second_number = (2) 
+
+
+  sum = first_number + second_number
+  difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number / second_number
+ 
+# puts get_variable_from_file(1, 2)
